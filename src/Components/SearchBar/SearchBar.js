ca1a9d2ef9dae145e400806b09ba60d4edb6f6c3
Javi Ortega <rotegras@gmail.com>
2020-12-24 17:06:05 +0100

    refactor SearchBar component as functional component

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,51 +3,38 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 
-class SearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      search: '',
-    }
+const SearchBar = ({ updateSearch, search }) => {
+
+  const onUpdateSearch = (value) => {
+    updateSearch(value);
   }
 
-  updateSearch = (value) => {
-    this.props.updateSearch(value);
-  }
-
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
-    this.setState({ search: value },
-      this.updateSearch(value)
-    )
+    onUpdateSearch(value)
   }
 
-  moveBookToShelf = (book, shelf) => {
-    this.props.moveBookToShelf(book, shelf);
-  }
-
-  render() {
-    return (
-      <div className="search-books-bar">
-        <Link to="/">
-          <button className="close-search">Close</button>
-        </Link>
-        <div className="search-books-input-wrapper">
-          <input
-            type="text"
-            value={this.state.search}
-            placeholder="Search by title or author"
-            onChange={(e) => this.handleChange(e)}
-            autoFocus
-          />
-        </div>
+  return (
+    <div className="search-books-bar">
+      <Link to="/">
+        <button className="close-search">Close</button>
+      </Link>
+      <div className="search-books-input-wrapper">
+        <input
+          type="text"
+          value={search}
+          placeholder="Search by title or author"
+          onChange={(e) => handleChange(e)}
+          autoFocus
+        />
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 SearchBar.propTypes = {
-  moveBookToShelf: PropTypes.func.isRequired,
+  updateSearch: PropTypes.func.isRequired,
+  search: PropTypes.string.isRequired,
 }
 
 
