baff0435f39354f6e2ed06c19f4f3b4f551e1deb
Javi Ortega <rotegras@gmail.com>
2020-12-24 11:17:16 +0100

    refactor: store shelves as constant

---
+++ src/constants/constants.js
@@ -0,0 +1,17 @@
+const SHELVES = [
+  {
+    id: 'wantToRead',
+    name: 'Want to Read',
+  },
+  {
+    id: 'currentlyReading',
+    name: 'Currenty Reading',
+  },
+  {
+    id: 'read',
+    name: 'Read',
+  }
+]
+
+
+export default SHELVES;
