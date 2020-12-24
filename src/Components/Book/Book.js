99207b23307b04b3e2aca297d533972e66ba74d7
Javi Ortega <rotegras@gmail.com>
2020-12-24 17:12:12 +0100

    style: clean code

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import * as BooksAPI from '../../BooksAPI';
 import PropTypes from 'prop-types';
+import * as BooksAPI from '../../BooksAPI';
 
 
 const Book = ({ book, moveBookToShelf, isSearchPage }) => {
